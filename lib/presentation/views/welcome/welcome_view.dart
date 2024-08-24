d245a524bf1406bb284de11bfd4cbcae283afb20
edwinyoo44 <7691822+edwinyoo44@users.noreply.github.com>
2024-08-24 19:17:35 +0800

    feat: 加入警告示意

--- lib/presentation/views/welcome/welcome_view.dart
+++ lib/presentation/views/welcome/welcome_view.dart
@@ -39,131 +39,163 @@ class WelcomeView extends StatelessWidget {
                 numberOfParticles: 64,
               ),
               // 前景內容
-              Container(
-                padding: const EdgeInsets.all(20),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // 標題
-                      Text(
-                        S.current.WelcomeTitle,
-                        style: const TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          shadows: [
-                            Shadow(
-                              blurRadius: 10.0,
-                              color: Colors.black,
-                              offset: Offset(5.0, 5.0),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Divider(),
-                      // 說明
-                      Text(
-                        S.current.WelcomeContent,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          shadows: [
-                            Shadow(
-                              blurRadius: 10.0,
-                              color: Colors.black,
-                              offset: Offset(5.0, 5.0),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 32),
-                      Container(
-                        padding: const EdgeInsets.all(16),
-                        color: Colors.white,
-                        child: Column(
-                          children: [
-                            Text(
-                              S.current.Language,
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                            // 下拉選單
-                            DropdownButton<Locale>(
-                              value: model.currentLocale,
-                              items: const [
-                                DropdownMenuItem(
-                                  value: Locale('en'),
-                                  child: Text('English'),
-                                ),
-                                DropdownMenuItem(
-                                  value: Locale.fromSubtags(
-                                    languageCode: 'zh',
-                                    scriptCode: 'Hant',
-                                    countryCode: 'TW',
-                                  ),
-                                  child: Text('繁體中文(台灣)'),
-                                ),
-                              ],
-                              onChanged: model.setCurrentLocale,
-                            ),
-                            Text(
-                              S.current.ApiUrl,
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                            // 輸入框
-                            TextField(
-                              controller: model.textController,
-                              decoration: InputDecoration(
-                                hintText: 'https://api.meshsight.example',
-                                hintStyle: const TextStyle(color: Colors.grey),
-                                enabledBorder: const OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white),
-                                ),
-                                focusedBorder: const OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.green),
-                                ),
-                                errorBorder: const OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.red),
-                                ),
-                                errorText: model.errorMessage,
-                                errorStyle: const TextStyle(
-                                  color: Colors.red,
-                                  fontWeight: FontWeight.bold,
-                                ),
+              SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(32),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // 標題
+                        Text(
+                          S.current.WelcomeTitle,
+                          style: const TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 10.0,
+                                color: Colors.black,
+                                offset: Offset(5.0, 5.0),
                               ),
-                              cursorColor: Colors.white,
-                              keyboardType: TextInputType.number,
-                              textAlign: TextAlign.center,
-                              onChanged: model.validateApiUrl,
-                            ),
-                            TextButton(
-                              onPressed: () => model.showOptionsDialog(context),
-                              child: Text(S.current.SelectOurDemoApi),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 32),
-                      // 按鈕 TRUE
-                      ElevatedButton.icon(
-                        onPressed: () async {
-                          await model.setAppSettingApi(
-                            AppSettingApi(
-                              apiUrl: model.textController.text,
-                            ),
-                          );
-                          onResult(true);
-                        },
-                        icon: const Icon(Icons.check, color: Colors.white),
-                        label: Text(S.current.Start),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          textStyle: const TextStyle(fontSize: 20),
+                        const Divider(),
+                        // 說明
+                        Text(
+                          S.current.WelcomeContent,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 10.0,
+                                color: Colors.black,
+                                offset: Offset(5.0, 5.0),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 32),
+                        Container(
+                          padding: const EdgeInsets.all(16),
+                          color: Colors.white,
+                          child: Column(
+                            children: [
+                              Text(
+                                S.current.Language,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                              // 下拉選單
+                              DropdownButton<Locale>(
+                                value: model.currentLocale,
+                                items: const [
+                                  DropdownMenuItem(
+                                    value: Locale('en'),
+                                    child: Text('English'),
+                                  ),
+                                  DropdownMenuItem(
+                                    value: Locale.fromSubtags(
+                                      languageCode: 'zh',
+                                      scriptCode: 'Hant',
+                                      countryCode: 'TW',
+                                    ),
+                                    child: Text('繁體中文(台灣)'),
+                                  ),
+                                ],
+                                onChanged: model.setCurrentLocale,
+                              ),
+                              Text(
+                                S.current.ApiUrl,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                              // 輸入框
+                              TextField(
+                                controller: model.textController,
+                                decoration: InputDecoration(
+                                  hintText: 'https://api.meshsight.example',
+                                  hintStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  enabledBorder: const OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                  focusedBorder: const OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.green),
+                                  ),
+                                  errorBorder: const OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.red),
+                                  ),
+                                  errorText: model.errorMessage,
+                                  errorStyle: const TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                cursorColor: Colors.white,
+                                keyboardType: TextInputType.number,
+                                textAlign: TextAlign.center,
+                                onChanged: model.validateApiUrl,
+                              ),
+                              TextButton(
+                                onPressed: () =>
+                                    model.showOptionsDialog(context),
+                                child: Text(S.current.SelectOurDemoApi),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 32),
+                        ElevatedButton.icon(
+                          onPressed: () async {
+                            await model.setAppSettingApi(
+                              AppSettingApi(
+                                apiUrl: model.textController.text,
+                              ),
+                            );
+                            onResult(true);
+                          },
+                          icon: const Icon(Icons.check, color: Colors.white),
+                          label: Text(
+                            S.current.AcceptAlertAndStart,
+                            style: const TextStyle(
+                                color: Colors.white), // 設置文字顏色為白色
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.redAccent,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                          ),
+                        ),
+                        const SizedBox(height: 32),
+                        // 警告訊息
+                        Container(
+                          padding: const EdgeInsets.all(16),
+                          color: Colors.red,
+                          child: Text(
+                            S.current.WelcomeAlert1,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        // 警告訊息
+                        Container(
+                          padding: const EdgeInsets.all(16),
+                          color: Colors.red,
+                          child: Text(
+                            S.current.WelcomeAlert2,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
