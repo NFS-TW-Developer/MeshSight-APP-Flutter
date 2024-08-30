0b3323606f6044f11ae65ea01c3f963f5cd13713
edwinyoo44 <7691822+edwinyoo44@users.noreply.github.com>
2024-08-30 20:36:42 +0800

    feat: index 中的說明彈窗內容應該要能滑動

--- lib/presentation/views/index/index_view_model.dart
+++ lib/presentation/views/index/index_view_model.dart
@@ -14,39 +14,41 @@ class IndexViewModel extends BaseViewModel {
         builder: (context) {
           return AlertDialog(
             title: Text(S.current.DevelopAlertTitle),
-            content: Column(
-              children: [
-                Text(S.current.DevelopAlertContent),
-                const SizedBox(height: 10),
-                TextButton(
-                  onPressed: () {
-                    launchUrl(Uri.parse(
-                        'https://github.com/edwinyoo44/MeshSight/issues'));
-                  },
-                  child: const Text(
-                      'https://github.com/edwinyoo44/MeshSight/issues'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    launchUrl(Uri.parse(
-                        'https://github.com/edwinyoo44/MeshSight-APP-Flutter/issues'));
-                  },
-                  child: const Text(
-                      'https://github.com/edwinyoo44/MeshSight-APP-Flutter/issues'),
-                ),
-                // 警告訊息
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  color: Colors.red,
-                  child: Text(
-                    "${S.current.WelcomeAlert1} \n\n ${S.current.WelcomeAlert2}",
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(S.current.DevelopAlertContent),
+                  const SizedBox(height: 10),
+                  TextButton(
+                    onPressed: () {
+                      launchUrl(Uri.parse(
+                          'https://github.com/edwinyoo44/MeshSight/issues'));
+                    },
+                    child: const Text(
+                        'https://github.com/edwinyoo44/MeshSight/issues'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      launchUrl(Uri.parse(
+                          'https://github.com/edwinyoo44/MeshSight-APP-Flutter/issues'));
+                    },
+                    child: const Text(
+                        'https://github.com/edwinyoo44/MeshSight-APP-Flutter/issues'),
+                  ),
+                  // 警告訊息
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    color: Colors.red,
+                    child: Text(
+                      "${S.current.WelcomeAlert1} \n\n ${S.current.WelcomeAlert2}",
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             actions: <Widget>[
               TextButton(
